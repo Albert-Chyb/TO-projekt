f3a0a54013119238bf4b7422ba8b1ef63c7d8076
Albert-Chyb <albert7773000@gmail.com>
2024-03-24 14:20:51 +0100

    Create middleware for db connection

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,29 @@
 const express = require('express');
+const mysql = require('mysql2/promise');
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-	res.send('Hello World!');
+app.use(async (req, res, next) => {
+	try {
+		const connection = await mysql.createConnection({
+			host: 'localhost',
+			user: 'root',
+			password: '123456',
+			database: 'test',
+		});
+
+		req.db = connection;
+		next();
+	} catch (error) {
+		next(error);
+	}
 });
 
-app.listen(port, () => {
+app.get('/', async (req, res) => {
+	res.send('Hello World');
+});
+
+app.listen(port, async () => {
 	console.log(`Server is running on port ${port}`);
 });
