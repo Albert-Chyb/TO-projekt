2a8aa62a52f468da097475d61df1d2dc5cbc6f8c
Albert Chyb <albert7773000@gmail.com>
2024-04-12 19:40:13 +0200

    Add database connection initialization

--- backend/index.ts
+++ backend/index.ts
@@ -1,4 +1,5 @@
 import express, { Express } from 'express';
+import { connectToDb } from './helpers/db-connection-singelton';
 import { mySQLConnection } from './middleware/db-connection';
 import columnsRoute from './routes/columns';
 import tablesRoute from './routes/tables';
@@ -9,6 +10,15 @@ const port = 3000;
 app.use(mySQLConnection);
 app.use('/tables', tablesRoute);
 app.use('/columns', columnsRoute);
+
+connectToDb({
+	host: 'localhost',
+	user: 'root',
+	password: '123',
+	database: 'shop',
+}).then(() => {
+	console.log('Connected to database');
+});
 
 app.listen(port, async () => {
 	console.log(`Server is running on port ${port}`);
