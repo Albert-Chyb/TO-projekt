2c10bfd780efc459a3147d9e9e81ca1988e02dac
Albert Chyb <albert7773000@gmail.com>
2024-04-13 00:38:55 +0200

    Add an endpoint that sets up database connection

--- backend/index.ts
+++ backend/index.ts
@@ -1,5 +1,4 @@
 import express, { Express } from 'express';
-import { connectToDb } from './helpers/db-connection-singelton';
 import { mySQLConnection } from './middleware/db-connection';
 import columnsRoute from './routes/columns';
 import dbConfigRoute from './routes/db-config';
@@ -8,20 +7,11 @@ import tablesRoute from './routes/tables';
 const app: Express = express();
 const port = 3000;
 
+app.use(express.json());
+app.use('/db-config', dbConfigRoute);
 app.use(mySQLConnection);
 app.use('/tables', tablesRoute);
 app.use('/columns', columnsRoute);
-app.use('/db-config', dbConfigRoute);
-
-connectToDb({
-	host: 'localhost',
-	user: 'root',
-	password: '123',
-	database: 'shop',
-	port: 3306,
-}).then(() => {
-	console.log('Connected to database');
-});
 
 app.listen(port, async () => {
 	console.log(`Server is running on port ${port}`);
--- backend/routes/db-config.ts
+++ backend/routes/db-config.ts
@@ -1,5 +1,19 @@
-import { Router } from 'express';
+import { Request, Router } from 'express';
+import {
+	DbConnectionConfig,
+	connectToDb,
+} from '../helpers/db-connection-singelton';
 
 const router = Router();
+
+router.post('/', async (req: Request<{}, {}, DbConnectionConfig>, res) => {
+	try {
+		await connectToDb(req.body);
+
+		return res.send(true);
+	} catch (error) {
+		return res.status(500).json(error as any);
+	}
+});
 
 export default router;
