4203ef5b8a4f089bef274e5928da563a3cff202c
Albert Chyb <albert7773000@gmail.com>
2024-04-12 19:40:25 +0200

    Refactor database connection middleware to use a singleton helper

--- backend/middleware/db-connection.ts
+++ backend/middleware/db-connection.ts
@@ -1,22 +1,15 @@
 import { RequestHandler } from 'express';
-
-import mysql from 'mysql2/promise';
+import { getDbConnection } from '../helpers/db-connection-singelton';
 
 /**
- * This middleware creates a connection to a MySQL database and attaches it to the request object.
+ * This middleware attaches the database connection to the request object.
  * The property name is `db`.
  */
 
 export const mySQLConnection: RequestHandler = async (req, res, next) => {
 	try {
-		const connection = await mysql.createConnection({
-			host: 'localhost',
-			user: 'root',
-			password: '123456',
-			database: 'test',
-		});
+		req.db = getDbConnection();
 
-		req.db = connection;
 		next();
 	} catch (error) {
 		next(error);
