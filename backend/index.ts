2536edf269d9814019ed18940aac304a07536e6b
Albert Chyb <albert7773000@gmail.com>
2024-04-12 21:12:11 +0200

    Add route for configuring database connection

--- backend/index.ts
+++ backend/index.ts
@@ -2,6 +2,7 @@ import express, { Express } from 'express';
 import { connectToDb } from './helpers/db-connection-singelton';
 import { mySQLConnection } from './middleware/db-connection';
 import columnsRoute from './routes/columns';
+import dbConfigRoute from './routes/db-config';
 import tablesRoute from './routes/tables';
 
 const app: Express = express();
@@ -10,12 +11,14 @@ const port = 3000;
 app.use(mySQLConnection);
 app.use('/tables', tablesRoute);
 app.use('/columns', columnsRoute);
+app.use('/db-config', dbConfigRoute);
 
 connectToDb({
 	host: 'localhost',
 	user: 'root',
 	password: '123',
 	database: 'shop',
+	port: 3306,
 }).then(() => {
 	console.log('Connected to database');
 });
